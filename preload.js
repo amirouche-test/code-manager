e982c9732ec57d2d06e102e026f99158df5c217a
amirouche-test <delmiam24@gmail.com>
2025-07-24 13:35:36 +0100

    update

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require('electron');
-const Store = require('electron-store');
-
-const store = new Store({ name: 'codes-data' });
-
-contextBridge.exposeInMainWorld('api', {
-  getCodes: () => store.get('codes', []),
-  saveCodes: (codes) => store.set('codes', codes)
-});
